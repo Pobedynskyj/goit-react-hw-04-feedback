d8d205cd250be1266b787ff59a65a72c9f3caf80
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2023-01-11 18:51:53 +0200

    create hooks in feedback

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,38 @@
+import s from './Feedback.module.css';
+import { useState } from 'react';
+import Statistics from '../Statistics/Statistics';
+
+export default function Feedback() {
+  const [{ good, neutral, bad }, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleOnClick = e => {
+    const name = e.target.name;
+    return setState(prevState => ({
+      ...prevState,
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  return (
+    <>
+      <h3 className={s.title}>Please leave feedback</h3>
+      <div className={s.btnTeam} onClick={handleOnClick}>
+        <button id={s.btn1} type="button" name="good" className={s.btn}>
+          Good
+        </button>
+        <button id={s.btn2} type="button" name="neutral" className={s.btn}>
+          Neutral
+        </button>
+        <button id={s.btn3} type="button" name="bad" className={s.btn}>
+          Bad
+        </button>
+      </div>
+
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </>
+  );
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,23 @@
+import s from './Statistics.module.css';
+
+export default function Statistics({ good, bad, neutral }) {
+  let totalValue = good + neutral + bad;
+  let totalPercent = ((good / totalValue) * 100).toFixed(1);
+
+  return (
+    <>
+      <h3 className={s.titleStat}>Statistics</h3>
+      {!totalValue ? (
+        <p className={s.notiStat}>No feedback given</p>
+      ) : (
+        <ul className={s.listTeam}>
+          <li className={s.itemStat}>Good: {good}</li>
+          <li className={s.itemStat}>Neutral: {neutral}</li>
+          <li className={s.itemStat}>Bad: {bad}</li>
+          <li className={s.itemStat}>Total: {totalValue}</li>
+          <li className={s.itemStat}>Positive feedback: {totalPercent}%</li>
+        </ul>
+      )}
+    </>
+  );
+}
